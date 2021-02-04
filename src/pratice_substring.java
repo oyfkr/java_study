59a94af1d219c562dc66f04ae2736038d42949ba
oyfkr <oyfkr@naver.com>
2021-02-04 15:40:14 +0900

    자바 substring공부

---
+++ src/pratice_substring.java
@@ -0,0 +1,12 @@
+public class pratice_substring {
+    public static void main(String[] args) {
+        String ex = "0123456789";
+        String tmp ="";
+
+        tmp = ex.substring(0,5);
+        System.out.println(tmp);
+
+        tmp = ex.substring(5);
+        System.out.println(tmp);
+    }
+}
